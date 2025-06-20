8611a49bbe91c69e474cd94bae4717849e183ba6
Eric Rodriguez <Eric Rodriguez@ERIC-RODRIGUEZ>
2025-06-20 08:39:39 -0600

    Avance

---
+++ src/labprogramacion_s9/MainJTunes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogramacion_s9;
+
+/**
+ *
+ * @author Eric Rodriguez
+ */
+public class MainJTunes {
+    public static void main(String[] args) {
+        Menuprincipal visualizar = new Menuprincipal();
+        visualizar.setVisible(true);
+        visualizar.setLocationRelativeTo(null);
+    }
+}
