d6ba7d5aca8e42d30604f0d2f40da9b509f91b27
Eric Rodriguez <Eric Rodriguez@ERIC-RODRIGUEZ>
2025-06-20 08:27:54 -0600

    Avance

---
+++ src/labprogramacion_s9/Song.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogramacion_s9;
+
+/**
+ *
+ * @author Eric Rodriguez
+ */
+public class Song {
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private int sumaEstrellas;
+    private int totalReviews;
+    private ImageIcon, imagenDisco; 
+}
